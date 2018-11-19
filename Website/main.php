b9eafd09f6ac849ff30b7a36e79d4c3de69d7f0a
Gabriel Ong <ryer168@gmail.com>
2018-11-19 17:51:34 -0500

    initial commit

---
+++ Website/main.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="style.css">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+<body>
+	<h1>Logged In!</h1>
+</body>
+</html>

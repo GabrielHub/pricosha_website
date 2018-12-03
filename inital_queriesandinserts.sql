f552d3dfc286d01cc42867d9cf418dc154cf26c2
Gabriel Ong <ryer168@gmail.com>
2018-12-02 22:05:30 -0500

    added logout function

--- inital_queriesandinserts.sql
+++
@@ -1,3 +0,0 @@
---For testing the login page
-INSERT INTO Person
-VALUES ("test@gmail.com", "password", "Dolph", "Lundgren");
--- source/main.php
+++ source/main.php
@@ -1,4 +1,13 @@
+<?php //How to check if logged in
+//init session
+session_start();
 
+//checking if logged in
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
 <html>
 	<head>
 		<link rel="stylesheet" href="style.css">
@@ -7,9 +16,12 @@
 	</head>
 <body>
 	<h1>MySpace 2.0</h1>
-	<p>Current features: Press the generate button to create accounts.</p>
+	<!--<p>Current features: Press the generate button to create accounts.</p>
 	<p>Press delete to delete these accounts.</p>
 	<button onclick = "window.location.href='insert.php'">Insert Initial Testing Accounts</button>
-	<button onclick = "window.location.href='delete.php'">Delete Testing Accounts</button>
+	<button onclick = "window.location.href='delete.php'">Delete Testing Accounts</button>-->
+	<p>
+        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+    </p>
 </body>
 </html>
